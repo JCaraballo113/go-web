cbe3336a9ce0fd558a590f194373c8297840a4a2
John Caraballo <jcaraballo019@gmail.com>
2021-12-16 15:38:37 -0500

    add template cache to take advantage of go templates

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,16 +1,75 @@
 package render
 
 import (
+	"bytes"
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 )
 
+var functions = template.FuncMap{}
+
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil) 
+	tc, err := CreateTemplateCache()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	t, ok := tc[tmpl]
+
+	if !ok {
+		log.Fatal("Template does not exist")
+	}
+
+	buf := new(bytes.Buffer)
+
+	_ = t.Execute(buf, nil)
+
+	_, err = buf.WriteTo(w)
 
 	if err != nil {
 		fmt.Println("Error parsing html",err)
 	}
+}
+
+// CreateTemplateCache creates a template cache as a map
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	templateCache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./templates/*.page.html")
+
+	if err != nil {
+		return templateCache, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+
+		if err != nil {
+			return templateCache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.html")
+
+		if err != nil {
+			return templateCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.html")
+
+			if err != nil {
+				return templateCache, err
+			}
+		}
+
+		templateCache[name] = ts
+	}
+
+	return templateCache, nil
 }
