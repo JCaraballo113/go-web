b0c90e2b556aef68b0569568e49c21d6d4d6fde8
John Caraballo <jcaraballo019@gmail.com>
2021-12-18 16:28:07 -0500

    Add CSRF protection, sessions  and routing

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -5,13 +5,18 @@ import (
 	"go-web/pkg/handlers"
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	mux := chi.NewRouter()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux.Use(middleware.Recoverer)
+	mux.Use(Nosurf)
+	mux.Use(SessionLoad)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 	return mux
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module go-web
 
 go 1.17
 
-require github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // indirect
+require (
+	github.com/alexedwards/scs/v2 v2.5.0
+	github.com/go-chi/chi v1.5.4
+	github.com/justinas/nosurf v1.1.1
+)
